cdb2df33231e8b9f67375ed99a357eb8a4d8ec99
soo-park <sparkstacks@gmail.com>
2017-10-22 12:33:38 -0700

    code cleanup

--- README.md
+++ README.md
@@ -4,21 +4,21 @@
     
 (X) Column 1 (labeled as col1 is the index column where the values are 1 to 1 million)
     
-( ) The next 9 columns (2 to 10) contain randomly distributed gaussian data 
+(X) The next 9 columns (2 to 10) contain randomly distributed gaussian data 
 
-( ) and are labelled col2_x ... col10_x where 'x' is the mean of the gaussian distribution.
+(X) and are labelled col2_x ... col10_x where 'x' is the mean of the gaussian distribution.
 For example, if you chose means of 10, 20, 30, 40, 50, 60, 70, 80 for col2 through col10, the labels are col2_10, col3_20, col4_30 and etc. You can choose whatever mean and variance you would like for each column.
 
-(I/P) Additionally, each of these columns have 10% nulls, randomly distributed.
+(X) Additionally, each of these columns have 10% nulls, randomly distributed.
 
-(I/P) Columns 11 to 19 are labelled as col11...col19, 
+(X) Columns 11 to 19 are labelled as col11...col19, 
 where each column has random strings selected from the English Dictionary.
 
-(I/P) 10% randomly distributed nulls in this column as well.
+(X) 10% randomly distributed nulls in this column as well.
 
-( ) Column 20 has random dates selected between January 1, 2014 to December 31, 2014.
+(X) Column 20 has random dates selected between January 1, 2014 to December 31, 2014.
 No nulls in this column.
 
-(I/P) Once this dataset has been created, load it into a single table in a sqlite database (ayasdi_assignment.db).
+(X) Once this dataset has been created, load it into a single table in a sqlite database (ayasdi_assignment.db).
 
 * Do not use 3rd party packages. For example, do not use numpy, pandas, and SQLAlchemy.
--- ayasdi_python_code.py
+++ ayasdi_python_code.py
@@ -7,11 +7,10 @@ import sqlite3
 from sqlite3 import Error
 from wordlist import wordlist
 
-ROWS = 1000000
+ROWS = 10
 
 def display_indicator(rows, anchor, message):
-    """
-        prints statement evey 10% of the rows to show how much has progressed
+    """ prints statement evey 10% of the rows to show how much has progressed
     """
 
     indicator = rows/10
@@ -21,8 +20,7 @@ def display_indicator(rows, anchor, message):
 
 
 def generate_header(start, end, attachment):
-    """
-        header generator
+    """ header generator
     """
 
     result = []
@@ -34,7 +32,8 @@ def generate_header(start, end, attachment):
 
 
 def generate_column(values, previous_table):
-    """Generate a column with given values"""
+    """ Generate a column with given values
+    """
 
     # result = []
 
@@ -50,8 +49,7 @@ def generate_column(values, previous_table):
 
 # Generating CSV file
 def generate_1st_column(rows):
-    """
-        labeled as col1
+    """ labeled as col1
         the index column where the values are 1 to 1 million
         (1 million rows with 1 header)
     """
@@ -62,16 +60,13 @@ def generate_1st_column(rows):
 
     for i in range(0, len(values)):
         result.append([values[i]])
-
-        # progress indicator
         display_indicator(ROWS, i, str(i) + " numbers processed for column 1")
 
     return result
 
 
 def get_random_num_given_per_null(max_number, percentage):
-    """
-        add 10% up of the number existing
+    """ add 10% up of the number existing
         if the number is over index, generate null
     """
 
@@ -85,11 +80,10 @@ def get_random_num_given_per_null(max_number, percentage):
 
 
 def generate_2nd_10th_column(rows):
-    """
-        The next 9 columns (2 to 10) contain randomly distributed gaussian data
+    """ The next 9 columns (2 to 10) contain randomly distributed gaussian data
         and are labelled col2_x ... col10_x where 'x' is the mean of the gaussian distribution.
-        For example, if you chose means of 10, 20, 30, 40, 50, 60, 70, 80, 90 for col2 through col10,
-        the labels are col2_10, col3_20, col4_30 and etc.
+        For example, if you chose means of 10, 20, 30, 40, 50, 60, 70, 80, 90 for
+        col2 through col10, the labels are col2_10, col3_20, col4_30 and etc.
         You can choose whatever mean and variance you would like for each column.
     """
 
@@ -104,10 +98,10 @@ def generate_2nd_10th_column(rows):
         std = 1
         column_to_attach = [["col" + str(column_index) + "_" + str(input_mean)]]
 
-        # paste a column to the previous column
         while len(column_to_attach) < rows + 1:
             item = []
             random_10per_null = get_random_num_given_per_null(1000, 0.1)
+
             if random_10per_null != None:
                 gauss_num = random.normalvariate(input_mean, std)
                 to_append = "{0:.2f}".format(gauss_num)
@@ -116,11 +110,9 @@ def generate_2nd_10th_column(rows):
             item.append(to_append)
             column_to_attach.append(item)
 
-            # progress indicator
             display_indicator(ROWS, \
                 len(column_to_attach), \
-                "column " + str(column_index) + ", " + str(len(column_to_attach)) + " processed"\
-            )
+                "column " + str(column_index) + ", " + str(len(column_to_attach)) + " processed")
 
         if result:
             result = generate_column(column_to_attach, result)
@@ -131,12 +123,9 @@ def generate_2nd_10th_column(rows):
 
 
 def get_random_word_10per_null():
-    """
-        returns word with 10% possiblity of returning null
+    """ returns word with 10% possiblity of returning null
     """
 
-    # word_dic = open('wordlist.txt', 'w')
-    # word_dic = file('wordlist.txt').read().split()
     word_dic = wordlist
     word_dic_length = len(word_dic) - 1
     random_word_location = get_random_num_given_per_null(word_dic_length, 0.1)
@@ -148,8 +137,7 @@ def get_random_word_10per_null():
 
 
 def append_data(start_col, end_col, rows, func_for_content):
-    """
-        append given data for the fows and columns given
+    """ append given data for the fows and columns given
     """
 
     result = [generate_header(start_col, end_col, "")]
@@ -161,20 +149,19 @@ def append_data(start_col, end_col, rows, func_for_content):
             item.append(to_append)
         result.append(item)
 
-        # progress indicator
         display_indicator(ROWS, len(result), \
         str(len(result)) + " data processed for " + str(func_for_content)[10:-1])
 
     return result
 
 
-# English word list downloaded from the following link
-# http://www-01.sil.org/linguistics/wordlists/english/
 def generate_11th_19th_column(rows):
-    """
-        Columns 11 to 19 are labelled as col11...col19,
+    """ Columns 11 to 19 are labelled as col11...col19,
         where each column has random strings selected from the English Dictionary.
         10% randomly distributed nulls in this column as well.
+
+        * English word list downloaded from the following link
+        http://www-01.sil.org/linguistics/wordlists/english/
     """
 
     start_col = 11
@@ -183,8 +170,7 @@ def generate_11th_19th_column(rows):
 
 
 def get_random_date():
-    """
-        generates random date
+    """ generates random date
     """
 
     start_date = "01/01/2014"
@@ -197,15 +183,14 @@ def generate_20th_column(rows):
     """
         Column 20 has random dates selected between January 1, 2014 to December 31, 2014.
         No nulls in this column.
+        * 0 - 364 (range to 365) are the random numbers to generate for 1 year
     """
 
-    # 0 - 364 (range to 365) are the random numbers to generate
     return append_data(20, 20, rows, get_random_date)
 
 
 def run_all(rows):
-    """
-    run all functions that generates the table
+    """ run all functions that generates the table
     """
 
     print "start row1"
@@ -214,7 +199,7 @@ def run_all(rows):
     second_10th = generate_2nd_10th_column(rows)
     print "add row 2 - 10"
     result = generate_column(second_10th, result)
-    print "start row 11 - 19"    
+    print "start row 11 - 19"
     eleventh_19th = generate_11th_19th_column(rows)
     print "add row 11 - 19"
     result = generate_column(eleventh_19th, result)
@@ -227,26 +212,21 @@ def run_all(rows):
     return result
 
 
-########################### generate csv
 def generate_csv_table(table_values):
-    """generate csv table with given array"""
+    """ generate csv table with given array
+    """
 
     with open('ayasdi_assignment.csv', 'wb') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',')
         filewriter.writerows(table_values)
 
 
-# ########################### Loading the CSV table to sqlite
 def create_connection(db_file):
-    """
-        create a database connection to the SQLite database
+    """ create a database connection to the SQLite database
         specified by the db_file
-        :param db_file: database file
-        :return: Connection object or None
     """
 
     try:
-        # con = sqlite3.connect(":memory:")
         conn = sqlite3.connect(db_file)
         return conn
     except Error as error:
@@ -256,15 +236,11 @@ def create_connection(db_file):
 
 
 def add_all_lines(conn, table_values):
-    """
-        add all lines in CSV to the SQLite database.
-        Query all rows in the tasks table
-        :param conn: the Connection object
-        :return:
+    """ add all lines to the SQLite database.
     """
 
     column_list = table_values[0]
-    column_row = filter(lambda x: x != "'", str(column_list)[1: -1])
+    column_row = ",".join(column_list)
     qmark = "?"
     col_count = len(column_list)
     for cols in range(1, col_count):
@@ -274,58 +250,50 @@ def add_all_lines(conn, table_values):
     cur = conn.cursor()
     cur.execute("DROP TABLE IF EXISTS ayasdi_table;")
     cur.execute("CREATE TABLE ayasdi_table (" + column_row + ");")
-
-    with open('ayasdi_assignment.csv', 'rb') as fin:
-        # `with` statement available in 2.5+
-        # csv.DictReader uses first line in file for column headings by default
-        dr_name = csv.DictReader(fin) # comma is default delimiter
-
-        to_db = [(i['col1'], i['col2_10'], i['col3_20'], i['col4_30'], i['col5_40'], i['col6_50'], \
-        i['col7_60'], i['col8_70'], i['col9_80'], i['col10_90'],i['col11'], i['col12'], i['col13'], i['col14'], \
-        i['col15'], i['col16'], i['col17'], i['col18'], i['col19'], i['col20']) for i in dr_name]
-
-    cur.executemany("INSERT INTO ayasdi_table (" + column_row + ") VALUES (" + qmark + ");", to_db)
+    cur.executemany(\
+        "INSERT INTO ayasdi_table (" + column_row + ") VALUES (" + qmark + ");", \
+        table_values)
 
 
-# def select_all_lines(conn):
-#     """
-#     Query all rows in the tasks table
-#     :param conn: the Connection object
-#     :return:
-#     """
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM ayasdi_table")
+def select_all_lines(conn):
+    """ Query all rows in the tasks table
+    """
 
-#     rows = cur.fetchall()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM ayasdi_table")
 
-#     for row in rows:
-#         print row
+    rows = cur.fetchall()
+
+    for row in rows:
+        print row
 
 
 def main():
-    """
-        connect with SQLite
+    """ connect with SQLite
     """
 
-    print "generate all columns:"
+    print "generate all columns in csv"
     table_values = run_all(ROWS)
     generate_csv_table(table_values)
 
+    print "creating a database connection"
     database = "./ayasdi_assignment.db"
-    # create a database connection
     conn = create_connection(database)
+
+    print "adding all lines to db"
     with conn:
-        # select_all_lines(conn)
-        print "adding all lines to db"
         add_all_lines(conn, table_values)
         print "commiting all lines"
         conn.commit()
         print "committed"
+        # print "Displaying all lines from db"
+        # select_all_lines(conn)
+        # print "finised displaying all lines from db"
 
+    print "closing connection"
     conn.close()
     print "connection closed"
 
 
 if __name__ == '__main__':
     main()
-
